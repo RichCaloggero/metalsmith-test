6949f9e6f60c3bbaea652d6b874594d2f2ccdd90
Rich Caloggero <rjc@mit.edu>
2022-03-17 16:37:38 -0400

    * now both client and server use a table to register socket events
    - unfortunately, node doesn't make it easy to use both es modules and commonJS simultaneously, so two copies of socketEvents.js exist, one for client and one for server
    
    - they are probably already out of sync since I added a bunch of logging to the server side

---
+++ backend/socketEvents.js
@@ -0,0 +1,35 @@
+
+module.exports = function registerSocketEvents (socket, descriptors) {
+socket.onAny((event, ...args) => {
+const descriptor = descriptors[event];
+if (!descriptor) return;
+console.log("received event ", event);
+if (descriptor instanceof Function) {
+// no response required, so just execute the handler
+descriptor(socket, ...args);
+} else if (descriptor instanceof Object && descriptor.handler) {
+console.log("- response required: ", descriptor.response);
+sendResponse(socket, descriptor.handler(socket, ...args), descriptor.response);
+} // if
+}); // connectAny
+} // registerSocketEvents
+
+function sendResponse (socket, handlerResponse, response) {
+if (response) {
+if (typeof(response) === "string" && handlerResponse) {
+socket.emit(response, handlerResponse);
+console.log("response sent: ", response, handlerResponse);
+} else if (Array.isArray(response) && response.length > 0) {
+const [success, failure] = response;
+if (handlerResponse) {
+socket.emit(success, handlerResponse);
+console.log("- sent success: ", response, handlerResponse);
+} else if(failure) {
+socket.emit(failure);
+console.log("- sent failure: ", response, handlerResponse);
+} // if
+} // if
+} // if
+} // sendResponse
+
+
--- backend/socketServer-https.js
+++ backend/socketServer-https.js
@@ -16,6 +16,8 @@ cors: {origin: "http://localhost:8000"}
 const auth = require("./auth.js");
 const file = require("./file.js");
 const build = require("../build.js");
+const registerSocketEvents = require("./socketEvents.js");
+
 const activeSockets = new Map();
 
 express.static.mime.define({"text/javascript": "js"});
@@ -27,83 +29,78 @@ io.on("connection", socket => {
 console.log(`Connection established via ${socket.conn.transport.name}: ${socket.id}`);
 socket.conn.once("upgrade", () => {
 console.log(`${socket.id} upgraded transport to ${socket.conn.transport.name}`);
-});
-activeSockets.set(socket, {});
-
+}); // log connection
 socket.onAny((event, ...data) => {
 console.log("received: ", event, data);
-}); // onAny
+}); // log events
 
+activeSockets.set(socket, {}); // maintain state
 
-socket.on("requestLogin", login);
-socket.on("requestFile", sendFile);
-socket.on("requestFileUpdate", updateFile);
-socket.on("error", handleError);
-
-socket.on("disconnect", async (reason) => {
-console.log(`Socket ${socket.id} disconnected -  ${reason}`);
-activeSockets.delete(socket);
-});
-
-
-/// event handlers
-
-function login (data) {
-console.log("login: ", data);
-const user = auth.login(data.eMail, data.password);
-if (user) {
-activeSockets.get(socket).userInfo = user;
-activeSockets.get(socket).fileList = sendFileList(socket);
-
-} else {
-socket.emit("loginFailed");
-} // if
-} // login
-
-function sendFileList (socket) {
-if (validLogin()) {
-const fileList = file.getAllFiles("source");
-console.log("sendFileList: ", fileList.length, " files to send...");
-socket.emit("fileList", {fileList});
-return fileList;
-} else {
-return [];
-} // if
-} // sendFileList
-
-function sendFile (data) {
-if (validLogin()) {
-const fileList = activeSockets.get(socket).fileList;
-console.log("sendFile: fileList.length = ", fileList?.length);
-if (fileList.includes(data.name)) {
-socket.emit("fileContents", {name: data.name, contents: fs.readFileSync(data.name).toString()});
-console.log("sendFile: ", data);
-} // if
-} // if
-} // sendFile
-
-function updateFile (data) {
-if (validLogin()) {
-fs.writeFileSync(data.name, data.contents);
-build();
-socket.emit("updateSuccessful", {name: data.name});
-console.log("updateFile: ", data);
-} // if
-} // updateFile
-
-function handleError (data) {
-console.log("error: ", data);
-} // handleError
-
-function validLogin () {
-return activeSockets.get(socket).userInfo;
-} // validLogin
-
-}); // listen for events
+registerSocketEvents(socket, {
+requestLogin: {handler: login, response: ["loginComplete", "loginFailed"]},
+requestFileList: {handler: fileList, response: "fileList"},
+requestFile: {handler: fileContents, response: "fileContents"},
+requestFileUpdate: {handler: updateFile, response: "fileUpdateComplete"},
+error: handleSocketError,
+disconnect: handleSocketDisconnect
+}); // registerEvents
+}); // socket
 
 server.listen(3000, () => {
 console.log("Listening on port 3000");
 });
+
+
+
+/// event handlers
+
+function login (socket, data) {
+console.log("login: ", data);
+return activeSockets.get(socket).userInfo = auth.login(data.eMail, data.password);
+} // login
+
+function fileList (socket, data) {
+if (validLogin(socket)) {
+console.log("sending fileList");
+return (activeSockets.get(socket).fileList = getFileList(socket));
+} // if
+} // fileList
+
+function getFileList () {
+return file.getAllFiles("source");
+} // getFileList
+
+function fileContents (socket, data) {
+if (validLogin(socket)) {
+const fileList = activeSockets.get(socket).fileList;
+console.log("sendFile: fileList.length = ", fileList?.length);
+if (fileList.includes(data.name)) {
+return {name: data.name, contents: fs.readFileSync(data.name).toString()};
+} // if
+} // if
+} // fileContents
+
+function updateFile (socket, data) {
+if (validLogin(socket) && data.name) {
+fs.writeFileSync(data.name, data.contents);
+build();
+return {name: data.name};
+} // if
+} // updateFile
+
+function handleSocketError (socket, data) {
+console.log("error: ", data);
+} // handleError
+
+function handleSocketDisconnect (socket, data) {
+activeSockets.delete(socket);
+console.log(`socket ${socket.id} disconnected;  ${data}`);
+} // handleSocketDisconnect
+
+function validLogin (socket) {
+return activeSockets.get(socket).userInfo;
+} // validLogin
+
 
 function sendClient (req, res, next) {
 const url = path.parse(req.url);
